be64c5d4346be8c1573e4e03c415183a242815c6
Anubhav Singh <anubhavsinghbkj@gmail.com>
2026-05-20 15:53:08 +0530

    refactor: update project name and enhance AboutPage with fade-in animations

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,28 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, useLocation } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import AboutPage from './pages/AboutPage';
 import EntrepreneursPage from './pages/EntrepreneursPage';
 import Header from './components/layout/Header';
 import Footer from './components/layout/Footer';
 
-const App = () => (
-  <div className="min-h-screen flex flex-col">
-    <Header />
-    <main className="flex-grow container mx-auto px-6 py-8">
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/about" element={<AboutPage />} />
-        <Route path="/entrepreneurs" element={<EntrepreneursPage />} />
-      </Routes>
-    </main>
-    <Footer />
-  </div>
-);
+const App = () => {
+  const location = useLocation();
+  const isAbout = location.pathname === '/about';
+
+  return (
+    <div className="min-h-screen flex flex-col bg-white">
+      <Header />
+      <main className={`flex-grow ${isAbout ? '' : 'container mx-auto px-6 py-8'}`}>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/entrepreneurs" element={<EntrepreneursPage />} />
+        </Routes>
+      </main>
+      <Footer />
+    </div>
+  );
+};
 
 export default App;
